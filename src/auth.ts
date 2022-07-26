360e3638aac04e64236f84c233342790347d6697
adarsha45 <shresthaadarsha957@gmail.com>
2022-07-26 16:36:35 +0545

    adarsha bug fix

--- src/auth.ts
+++ src/auth.ts
@@ -4,12 +4,10 @@ import * as vscode from 'vscode';
 import * as data from "./json/awt.json";
 const {exec} = require("child_process");
 const path = require('path');
-
-
-
 const os = require('os');
 const fs = require('fs');
 export async function sfdxorgcreator() {  
+  //--------------------------------------------------------------
     let username:any;
     let password:any;
     let imageName:any;
@@ -19,6 +17,15 @@ export async function sfdxorgcreator() {
     let jobName:any;
     let jobToken:any;
     let REQ_INC = "ada";
+
+    let sfUsernamejson:any;
+let sfPasswordsjson:any;
+let sfImageNamejson:any;
+let sfImageTagjson:any;
+let sfjobtokenjson:any;
+let sfjobNamejson:any;
+let sfEnvironmentjson:any;
+let afterdata:any;
 //Read user previously stored data ---------------------------------
 
 // const beforedata =  fs.readFileSync('/home/adarsha/Documents/extension/sfdxExtension/CQ-scratch-org-creator/src/json/awt.json', 'utf8');
@@ -32,6 +39,7 @@ export async function sfdxorgcreator() {
          dirPath = `${__dirname}/cqconfig`;
          console.log(dirPath);
     } else {
+      console.log("directory doesnot exist");
        dirPath = path.join(__dirname, '/cqconfig');
       fs.mkdirSync(dirPath); }  
        let build:any = [{
@@ -52,6 +60,8 @@ export async function sfdxorgcreator() {
       //if Build with params 
 
       if(buildType.label === "Build With Params" ){
+        console.log("inside build with params");
+        console.log(buildType.label);
         let username:any = await vscode.window.showInputBox({
             prompt:'Enter Your UserName',
             placeHolder: 'Plese!! Enter Your username',   
@@ -149,7 +159,7 @@ export async function sfdxorgcreator() {
             sfjobToken:jobToken,
             sfImageName:imageName,
             sfImageTag:imageTag,
-            sfEnvironment:environ,
+            sfEnvironment:environ.label,
         } ;
         const stringify = JSON.stringify(newData);
     
@@ -158,10 +168,70 @@ export async function sfdxorgcreator() {
             // error checking
             if(err) {throw err;};        
         });
+        setTimeout(function(){
+          console.log(dirPath);
+          console.log('timeout function');
+         afterdata =  fs.readFileSync(`${dirPath}/cq.json`, {encoding:'utf8', flag:'r'});
+         console.log(afterdata);
+         jsonafterData = JSON.parse(afterdata);
+         console.log(jsonafterData);
+         //Get data from json file ------------------------------------
+         sfUsernamejson = jsonafterData.sfUsername;
+         sfPasswordsjson = jsonafterData.sfPassword;
+         sfjobNamejson = jsonafterData.sfjobName;
+         sfjobtokenjson = jsonafterData.sfjobToken;
+         sfImageNamejson = jsonafterData.sfImageName;
+         sfImageTagjson = jsonafterData.sfImageTag;
+         sfEnvironmentjson = jsonafterData.sfEnvironment;
+         console.log(sfImageTagjson);
+         console.log("reading data");
+         console.log(sfUsernamejson);
+         console.log(sfPasswordsjson);
+         console.log(sfjobNamejson);
+         console.log(sfjobtokenjson);
+         setTimeout(function () { jenkinsbuild(sfUsernamejson, sfPasswordsjson, sfjobNamejson, sfjobtokenjson, sfImageNamejson, sfImageTagjson, sfEnvironmentjson); }, 1000);
+        }, 2000);
     
+        function jenkinsbuild(sfUsernamejson:any, sfPasswordsjson:any, jobname:any, jobtoken:any, sfImageNamejson:any, sfImageTagjson:any, sfEnvironmentjson:any){
+          console.log("inside jenkins build ");
+          console.log(sfUsernamejson);
+          console.log(sfPasswordsjson);
+          console.log(jobname);
+          console.log(jobtoken);
+          console.log(sfImageNamejson);
+          console.log(sfImageTagjson);
+          console.log(sfEnvironmentjson);
+          var jenkinsapi = require('jenkins-api');
 
+
+          var jenkins = jenkinsapi.init(`http://${sfUsernamejson}:${sfPasswordsjson}@localhost:8080`);      
+            console.log(jenkins);              
+                    //specifying particular job name and its token
+                        console.log("jenkins param build");
+                        jenkins.build_with_params(`${jobname}`,{depth: 1, "IMAGE_NAME": `${sfImageNamejson}`,
+
+                         "IMAGE_TAG": `${sfImageTagjson}`,
+                         
+                         "ENVIROMENT": `${sfEnvironmentjson}`,
+                                                  
+                         "REQ_INC": `${REQ_INC}`,token:`${jobtoken}` }, function(err:any, data:any){
+                         
+                           if(err) {  
+                            console.log(err);
+                            vscode.window.showWarningMessage(`Your Build ${jobName} Has Failed : Try Again or Check The Input`);
+                          }
+                           else{
+                            console.log(data);
+                            vscode.window.showInformationMessage(`Your Build ${jobName} Has  Triggered Succesfully`);
+                           }
+                         
+                         
+                         });
+                    }
       }
       else{
+        console.log("inside normal buid");
+        console.log(buildType.label);
         username = await vscode.window.showInputBox({
             prompt:'Enter Your UserName',
             placeHolder: 'Plese!! Enter Your username',
@@ -222,71 +292,72 @@ export async function sfdxorgcreator() {
             // error checking
             if(err) {throw err;};        
         });
-      }
-//---------------------------------------------------------------------------------
-let sfUsernamejson:any;
-let sfPasswordsjson:any;
-let sfImageNamejson:any;
-let sfImageTagjson:any;
-let sfjobtokenjson:any;
-let sfEnvironmentjson:any;
-let sfjobNamejson:any;
-let afterdata:any;
 
         setTimeout(function(){
-         afterdata =  fs.readFileSync(`${dirPath}/cq.json`, 'utf8');
+          console.log(dirPath);
+          console.log('timeout function');
+         afterdata =  fs.readFileSync(`${dirPath}/cq.json`, {encoding:'utf8', flag:'r'});
+         console.log(afterdata);
          jsonafterData = JSON.parse(afterdata);
+         console.log(jsonafterData);
          //Get data from json file ------------------------------------
          sfUsernamejson = jsonafterData.sfUsername;
          sfPasswordsjson = jsonafterData.sfPassword;
          sfjobNamejson = jsonafterData.sfjobName;
          sfjobtokenjson = jsonafterData.sfjobToken;
-         sfImageNamejson = jsonafterData.sfImageName;
-         sfImageTagjson = jsonafterData.sfImageTag;
-         sfEnvironmentjson = jsonafterData.sfEnvironment;
-         console.log(sfImageTagjson);
-         setTimeout(function() { jenkinsbuild(sfUsernamejson,sfPasswordsjson,sfjobNamejson,sfjobtokenjson,sfImageNamejson,sfImageTagjson,sfEnvironmentjson); }, 1000);
+         console.log("reading data");
+         console.log(sfUsernamejson);
+         console.log(sfPasswordsjson);
+         console.log(sfjobNamejson);
+         console.log(sfjobtokenjson);
 
-        },1000);
+         setTimeout(function() { jenkinsbuild(sfUsernamejson,sfPasswordsjson,sfjobNamejson,sfjobtokenjson); }, 1000);
+        },2000);
 
         var jenkinsapi = require('jenkins-api');
 
-        function jenkinsbuild(sfUsernamejson:any,sfPasswordsjson:number | string,jobname:any,jobtoken:any,sfImageNamejson:any,sfImageTagjson:any,sfEnvironmentjson:any){
-            var jenkins = jenkinsapi.init(`https://${sfUsernamejson}:${sfPasswordsjson}@localhost:8080`);                    
+        function jenkinsbuild(sfUsernamejson:any,sfPasswordsjson:any,jobname:any,jobtoken:any){
+          console.log("inside jenkins build ");
+          console.log(sfUsernamejson);
+          console.log(sfPasswordsjson);
+          console.log(jobname);
+          console.log(jobtoken);
+
+          var jenkins = jenkinsapi.init(`http://${sfUsernamejson}:${sfPasswordsjson}@localhost:8080`);      
+            console.log(jenkins);              
                     //specifying particular job name and its token
-
-                    if(buildType.label === "Build With Params" ){
-                        console.log("jenkins param build");
-                        jenkins.build_with_params(`${jobname}`,{depth: 1, "IMAGE_NAME": `${sfImageNamejson}`,
-
-                         "IMAGE_TAG": `${sfImageTagjson}`,
-                         
-                         "ENVIROMENT": `${sfEnvironmentjson}`,
-                                                  
-                         "REQ_INC": `${REQ_INC}`,token:`${jobtoken}`, }, function(err:any, data:any){
-                         
-                           if(err) {  
-                            vscode.window.showWarningMessage(`Your Build ${jobName} Has Failed : Try Again or Check The Input`);
-                          }
-                           else{
-                            vscode.window.showInformationMessage(`Your Build ${jobName} Has  Triggered Succesfully`);
-                           }
-                         
-                         
-                         });
-                    }else{
                         console.log("jenkins normal build");
                         jenkins.build(`${jobname}`, {token:`${jobtoken}`}, function(err:any, data:any) {
                             if(err){
+
+                              console.log(err);
                               vscode.window.showWarningMessage(`Your Build ${jobName} Has Failed with status 400: Try Again or Check The Input`);
                             }
                             else{
-                                vscode.window.showInformationMessage(`Your Build ${jobname} Has Triggered succesfully with status 201`);
-                                
+                              console.log(data);
+                                vscode.window.showInformationMessage(`Your Build ${jobname} Has Triggered succesfully with status 201`);                                
                             }
                           });  
-                    }
-                                                     
         }
+      }
+//---------------------------------------------------------------------------------
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//----------------------------End------------------------------------
     }
         
